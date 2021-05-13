6ac6cb2b7c14ca03de5fb449b4f53570b25eacf1
juancereceda <juanmcereceda@gmail.com>
2021-05-13 12:08:01 -0300

    popcorn logo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
 import {addMovieFavorite,getMovies,removeMovieFavorite,getMovieDetail} from '../../actions/index'
 import './Home.css';
-
+import Popcorn from '../../popcorn.png'
 
 
 export default class Home extends Component {
@@ -21,7 +21,7 @@ export default class Home extends Component {
         <h1>Welcome to Juan Martin's MovieApp!</h1>
         <p>Hi there! My name is Juan Martín and this is my MovieApp. Here you will be able to access to detailed information about your favorite movies. You can also fav the movies you like in order to add them as your favorite movies, or make your personalized watchlist!</p>
           </div>   
-          <img src={'https://www.flaticon.es/svg/vstatic/svg/3439/3439699.svg?token=exp=1620860430~hmac=2404fe7f5b67ffddb41dc22cff4e3ef0'} height='120' width='120'/>
+          <img src={Popcorn} height='120' width='120'/>
       </div>
     );
   }
